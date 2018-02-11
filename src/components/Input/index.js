d29b0a5b25af94d59309453cc90710ef5047d8c2
Robert Oganiani <oganiani90@gmail.com>
2018-02-11 23:39:12 +0400

    Add complete Input component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const Input = () => (
+const Input = ({ value, disabled, onInputChange, handleOnAddClick }) => (
   <div>
-    <input type="text" />
-    <button>Add</button>
+    <input
+      value={value}
+      onChange={e => onInputChange(e.target.value)}
+      onKeyPress={e =>
+        e.key === 'Enter' && !disabled ? handleOnAddClick() : null
+      }
+      type="text"
+    />
+    <button
+      style={{ marginLeft: '10px' }}
+      disabled={disabled}
+      onClick={() => handleOnAddClick()}
+    >
+      Add
+    </button>
   </div>
 );
 
