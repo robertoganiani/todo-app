0f5ce3ac1077fd15c31be12118f2d39e3387d028
Robert Oganiani <oganiani90@gmail.com>
2018-02-11 17:45:35 +0400

    Add Input component

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
-import './app.css'
+import './app.css';
+import Input from '../../components/Input';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-
+        <Input />
       </div>
     );
   }
