d60a4998a0133b27edeedbd91f34ba09ba19c753
Robert Oganiani <robertoganiani@gmail.com>
2018-02-11 17:29:55 +0400

    Move App to its directory

---
+++ src/containers/App/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './app.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+
+      </div>
+    );
+  }
+}
+
+export default App;
